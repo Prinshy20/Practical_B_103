f468d85512416f6c83b71b692d33337352faf4b6
Prinshy20 <prinsipatel2008@gmail.com>
2026-02-23 22:16:45 +0530

    FullPyramid.java

--- FullPyramid.java
+++ FullPyramid.java
@@ -3,9 +3,18 @@ public class FullPyramid {
         int rows = 5;
 
         for (int i = rows; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
+
+            // Print leading spaces
+            for (int space = 0; space < rows - i; space++) {
+                System.out.print(" ");
+            }
+
+            // Print stars with space
+            for (int star = 0; star < i; star++) {
                 System.out.print("* ");
             }
+
+            // Move to next line
             System.out.println();
         }
     }
